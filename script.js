b5b3c73c65911853937f78201fb18d44fdf13801
Bittu <bittupathania234@gmail.com>
2024-01-12 20:34:27 +0530

    Completed the task

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let box = document.getElementById("Box");
+
+box.style.height = "200px";
+box.style.width =  "300px";
+box.style.backgroundColor = "yellow ";
+box.style.color = "blue";
+box.style.margin = "18px";
+box.style.padding = "1px";
+box.style.fontSize = "19px";
+box.style.fontWeight = "bold";
+
+document.getElementById("para").innerHTML = "Hello, I'm a div!"
